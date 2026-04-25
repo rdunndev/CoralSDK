5434386c6956a53609b5799903c7440f0f2a3dd1
rich-d-oe <richard.dunnicliffe@octopus.energy>
2026-04-25 08:17:01 +0100

    Corrected to use material3 button

--- coral/src/main/java/energy/octopus/coral/Button.kt
+++ coral/src/main/java/energy/octopus/coral/Button.kt
@@ -1,8 +1,9 @@
 package energy.octopus.coral
 
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Button(text: String, onClick : () -> Unit) {
-    Button(text = text, onClick = {onClick()})
+    Button(text = text) { }
 }
