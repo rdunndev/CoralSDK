0cb86bfb57138ecfa5446e2e17f6a44e6def9349
rich-d-oe <richard.dunnicliffe@octopus.energy>
2026-04-27 09:31:01 +0100

    Change from Button to CoralButton for testing to avoid cyclic calls.

--- coral/src/main/java/energy/octopus/coral/Button.kt
+++ coral/src/main/java/energy/octopus/coral/Button.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Button(text: String, onClick : () -> Unit) {
+fun CoralButton(text: String, onClick : () -> Unit) {
     Button(
         onClick = onClick,
     ) { Text(text) }
